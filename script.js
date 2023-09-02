7bd8180386bf33b3474139e36b91a9b74ded00c9
Francisco Wesner <franciscowesner@hotmail.com>
2023-09-02 19:07:30 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const userList = document.getElementById('user-list');
+
+  // Función para obtener y mostrar la lista de usuarios
+  async function fetchUsers() {
+    try {
+      const response = await fetch('/api/users');
+      if (!response.ok) {
+        throw new Error('Error al obtener usuarios');
+      }
+      const users = await response.json();
+      users.forEach(user => {
+        const listItem = document.createElement('li');
+        listItem.textContent = user.name;
+        userList.appendChild(listItem);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Llamar a la función para cargar la lista de usuarios
+  fetchUsers();
+});
